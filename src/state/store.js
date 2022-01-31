09792506c2ee00386f4031fcde806a57e1ea164f
od-c0d3r <omar_rashad@aol.com>
2022-01-31 14:33:14 +0200

    Setup: essintial middlewears

--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger'
+import { reduxBatch } from '@manaflair/redux-batch'
 import airReducer from './air/airReducer';
 
 export const store = configureStore({
   reducer: {
-    counter: airReducer,
+    air: airReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV !== 'production',
+  enhancers: [reduxBatch],
 });
