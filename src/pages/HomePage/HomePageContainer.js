cceaad935eb4767caf93ff5462c3770a50eccb80
od-c0d3r <omar_rashad@aol.com>
2022-02-02 10:58:30 +0200

    Style: finish HomePageContainer style

--- src/pages/HomePage/HomePageContainer.js
+++ src/pages/HomePage/HomePageContainer.js
@@ -1,3 +1,24 @@
-const HomePageContainer = () => <h1>#HomePageContainer</h1>;
+import { Card, Col, Row } from 'react-bootstrap';
+
+const HomePageContainer = () => {
+  const array = [{ id: 0, name: 'egypt', airIdx: 2 }, { id: 1, name: 'egypt', airIdx: 2 }, { id: 2, name: 'egypt', airIdx: 2 }, { id: 3, name: 'egypt', airIdx: 2 }, { id: 4, name: 'egypt', airIdx: 2 }];
+
+  return (
+    <Row xs={2} md={3} className="g-4 ms-0 me-0">
+      {array.map((country) => (
+        <Col key={country.id}>
+          <Card>
+            <Card.Body>
+              <Card.Title>{country.name}</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
 
 export default HomePageContainer;
