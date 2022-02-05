1b5b8353df42f49c7fe897c0a5e3ea56ed278386
od-c0d3r <omar_rashad@aol.com>
2022-02-05 14:18:56 +0200

    Feat: update api info

--- src/api/owAPI.js
+++ src/api/owAPI.js
@@ -1,7 +1,7 @@
 const BASE_API = 'http://api.openweathermap.org/data/2.5/air_pollution?';
 
 const getAirIndex = async (lon, lat) => {
-  const data = await fetch(`${BASE_API}lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OW_KEY}`);
+  const data = await fetch(`${BASE_API}lat=${lat}&lon=${lon}&appid=45997ba80da98040a7b69f317ccc26ab`);
   return data.json();
 };
 
