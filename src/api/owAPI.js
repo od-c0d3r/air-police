5c6158603589368d08d86c878aaaea769adf08c1
od-c0d3r <omar_rashad@aol.com>
2022-02-05 14:42:01 +0200

    Feat: use secure protocol to recive from api

--- src/api/owAPI.js
+++ src/api/owAPI.js
@@ -1,4 +1,4 @@
-const BASE_API = 'http://api.openweathermap.org/data/2.5/air_pollution?';
+const BASE_API = 'https://api.openweathermap.org/data/2.5/air_pollution?';
 
 const getAirIndex = async (lon, lat) => {
   const data = await fetch(`${BASE_API}lat=${lat}&lon=${lon}&appid=45997ba80da98040a7b69f317ccc26ab`);
